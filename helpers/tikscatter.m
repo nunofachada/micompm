3b825a5dc289afbc0ad5e7515e358d209bb3a8f0
Nuno Fachada <faken@fakenmc.com>
2016-01-14 18:38:56 +0000

    tikscatter: add proper docs + improve code style

--- helpers/tikscatter.m
+++ helpers/tikscatter.m
@@ -1,6 +1,21 @@
 function figstr = tikscatter(pts, groups, marks)
-%TIKSCATTER Summary of this function goes here
-%   Detailed explanation goes here
+% TIKSCATTER Creates a simple TikZ 2D scatter plot within a tikzpicture
+% environment. The points are plotted on a normalized figure with x and y
+% axes bounded between [-1, 1]. To render adequately, the final LaTeX
+% document should load the plotmarks TikZ library.
+%
+%   figstr = TIKSCATTER(pts, groups, marks)
+%
+% Parameters:
+%     pts - Points to plot, m x 2 numeric matrix, where m is the number of
+%           observations or points to plot.
+%  groups - Vector of integers determining to which group observations in
+%           pts belong to.
+%   marks - Optional cell array of strings which specify how to draw the
+%           marks in TikZ.
+%
+% Outputs:
+%  figstr - Handles of the generated plots, if any.
 %
 % Copyright (c) 2016 Nuno Fachada
 % Distributed under the MIT License (See accompanying file LICENSE or copy 
@@ -12,10 +27,10 @@ if nargin == 2
     marks = {...
         'mark=square*,mark options={color=red},mark size=0.8pt', ...
         'mark=*,mark size=0.6pt', ...
-        'mark=o,mark size=0.7pt'}; % Improve this
+        'mark=o,mark size=0.7pt'};
 end;
 
-% Normalize 
+% Normalize points
 max_pt = max(max(abs(pts(:, 1:2))));
 pts = pts / max_pt;
 
@@ -25,20 +40,24 @@ ugrps = unique(groups);
 % Begin figure
 figstr = '\begin{tikzpicture}[scale=6] \path (-1.2,-1.2) (1.2,1.2); ';
 
-figstr = sprintf('%s \\draw[very thin,color=gray] (0,1.1)--(0,-1.1); \\draw[very thin,color=gray] (1.1,0)--(-1.1,0);', figstr);
+% Draw axes
+figstr = sprintf(['%s \\draw[very thin,color=gray] (0,1.1)--(0,-1.1);' ...
+    '\\draw[very thin,color=gray] (1.1,0)--(-1.1,0);'], figstr);
 
 % Cycle groups
-for i=1:numel(ugrps)
+for i = 1:numel(ugrps)
     
     % Get points in group
-    pts_in_grp = pts(find(groups==i), 1:2);
+    pts_in_grp = pts(groups == i, 1:2);
 
     % Begin plotting points in current group
-    figstr = sprintf('%s \\path plot[%s] coordinates {', figstr, marks{i});
+    figstr = sprintf('%s \\path plot[%s] coordinates {', ...
+        figstr, marks{i});
 
     % Cycle points in group
     for j=1:size(pts_in_grp, 1)
-       figstr = sprintf('%s (%5.3f,%5.3f)', figstr, pts_in_grp(j, 1), pts_in_grp(j, 2));
+       figstr = sprintf('%s (%5.3f,%5.3f)', ...
+           figstr, pts_in_grp(j, 1), pts_in_grp(j, 2));
     end;
     
     % End current group
@@ -46,9 +65,8 @@ for i=1:numel(ugrps)
 
 end;
 
+% End figure
 figstr = sprintf('%s \\end{tikzpicture} ', figstr);
-
-
 
 end
 
