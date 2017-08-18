dda571bafff3aa97352d550b388d03c859183b24
Nuno Fachada <faken@fakenmc.com>
2017-08-18 16:03:51 +0100

    Check for presence of Statistics MATLAB toolbox or Octave package and for the latter, autoload said package

--- startup.m
+++ startup.m
@@ -4,7 +4,33 @@
 % Distributed under the MIT License (See accompanying file LICENSE or copy 
 % at http://opensource.org/licenses/MIT)
 %
+
+% Add paths containing source code
 addpath([pwd() filesep '3rdparty']);
 addpath([pwd() filesep 'helpers']);
 addpath([pwd() filesep 'micompm']);
+
+% Global variables
+global micompm_version;
+
+% micompm version
+micompm_version = '0.0.1';
+
+% Check if statistics toolbox/package is present
+if is_octave()
+    check = pkg('list', 'statistics');
+    if isempty(check)
+        error('micompm requires Octave ''statistics'' package.');
+    else
+        pkg('load', 'statistics');
+    end;
+else
+    check = license('test','Statistics_toolbox');
+    if ~check
+        error('Micompm requires MATLAB ''Statistics'' toolbox.');
+    end;
+end;
+
+% If no error, present welcome message
+fprintf('\n * Micompm v%s loaded\n\n', micompm_version);
 
